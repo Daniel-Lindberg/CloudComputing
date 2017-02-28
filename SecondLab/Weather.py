1e31ec649e07ea25ed978a03f4eef6cc305b72dc
Ubuntu <ubuntu@ip-172-31-39-121.us-west-2.compute.internal>
2017-02-28 22:11:59 +0000

    Final Second Lab

--- SecondLab/Weather.py
+++ SecondLab/Weather.py
@@ -19,9 +19,11 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 app.config.from_envvar('FNOC_SETTINGS', silent=True)
-
+import json
 import random
 import csv
+import os
+import re
 
 dates = []
 TMAXs = []
@@ -41,30 +43,59 @@ del TMINs[0]
 
 @app.route("/historical")
 # five test cases: n=10, n=-1, no parameter, malformed parameters
-def historical():
-	all_dates=""
+def historical():	
+	json_list = []
 	for date in dates:
-		all_dates += "Date:"+date+'\n'
-	return jsonify({'info': all_dates})
+		json_list.append({'Date':date})
+	return json.dumps(json_list)
 	
 @app.route('/historical/<date_id>',methods=['GET'])
 def get_date(date_id):
 	try:
+		json_list = []
 		value = dates.index(date_id)
-		info_message = "Date:"+dates[value]+" TMAX:"+TMAXs[value]+" TMIN:"+TMINs[value]
-		return (jsonify({'info': info_message}))
+		json_list.append({"Date":dates[value]})
+		json_list.append({"TMAX":TMAXs[value]})
+		json_list.append({"TMIN":TMINs[value]})
+		return json.dumps(json_list)
 	except:
 		error_message = "HTTP Error code 404"
 		return (jsonify({'error': error_message}))
 
-@app.route('/historical/data_info',methods=['POST'])
-def post_date(date_info):
-	print date_info
-	#TO ADD: get date info to give a date, tmax and tmin
+@app.route('/historical/<data_info>',methods=['POST'])
+def post_date(data_info):
+	date, temp_tmax, temp_tmin= request.get_data().split(',')
+	date_value = re.findall(r'\d+', date)
+	tmax_value = re.findall(r'\d+',temp_tmax)
+	tmin_value = re.findall(r'\d+',temp_tmin)
+	date_value = date_value[0]
+	tmax_value = tmax_value[0]
+	tmin_value = tmin_value[0]
 	with open(r'daily.csv','a') as csvfile:
-		spamwriter = csv.writer(csvfile)
-		spamwriter.writerow([])
-		spamwriter.writerow([date,tmax,tmin])
+                spamwriter = csv.writer(csvfile)
+                spamwriter.writerow([])
+                spamwriter.writerow([str(date_value),str(tmax_value),str(tmin_value)])	
+	return (jsonify({'Post':'Response'}))
+
+@app.route('/historical/<data_id>',methods=['DELETE'])
+def delete_date(data_id):
+	inputCSV=open('daily.csv','rb')
+	outputCSV=open('daily2.csv','wb')
+	writer=csv.writer(outputCSV)
+	deleted = False
+	for row in csv.reader(inputCSV):
+		if row[0]!='DATE':
+			if row[0] != data_id:
+				writer.writerow(row)
+			else:
+				value = dates.index(date_id)
+				del dates[value]
+				del TMAXs[value]
+				del TMINs[value]
+				deleted=True
+	os.system('cp daily2.csv daily.csv')
+	return jsonify({'Deleted Result':deleted})	
+
 
 @app.route('/forecast/<date_id>',methods=['GET'])
 def get_forecast(date_id):
@@ -74,23 +105,22 @@ def get_forecast(date_id):
 		t_max = TMAXs[value]
 		t_min = TMINs[value]
 		forecast = []
-		fun_string = 'Date:'+str(dates[value])
-		fun_string +=' TMAX:'+str(t_max)
-		fun_string +=' TMIN:'+str(t_min)+'\n'
+		fun_string =[]
+		fun_string.append({'Date':str(dates[value])})
+		fun_string.append({'TMAX':str(t_max)})
+		fun_string.append({'TMIN':str(t_min)})
 		forecast.append(fun_string)
 		for x in range(1,8):
 			new_days.append(int(dates[value])+x)
 		for x in new_days:
 			new_tmax = int(t_max)+random.randint(-3,3)
 			new_tmin = int(t_min)+random.randint(-3,3)
-			forecast_string = 'Date:'+str(x)
-			forecast_string +=' TMAX:'+str(new_tmax)
-			forecast_string +=' TMIN:'+str(new_tmin)+'\n'
+			forecast_string = []
+			forecast_string.append({'Date':str(x)})
+			forecast_string.append({'TMAX':str(new_tmax)})
+			forecast_string.append({'TMIN:':str(new_tmin)})
 			forecast.append(forecast_string)
-		info_message = ''
-		for x in forecast:
-			info_message+=x
-		return (jsonify({'info': info_message}))
+		return json.dumps(forecast)
 	except:
 		error_message = 'HTTP Error code 404'
 		return jsonify({'error':error_message})
--- SecondLab/testReadCSV.py
+++ SecondLab/testReadCSV.py
@@ -18,12 +18,25 @@ del date[0]
 del TMAX[0]
 del TMIN[0]
 
-import random
-print random.randint(0,3)
+#import random
+#print random.randint(0,3)
 
+date = '20150820'
+input1 = open('daily3.csv','rb')
+output = open('daily3.csv','wb')
+writer=csv.writer(output)
+for row in csv.reader(input1):
+	if row[0] != date:
+		writer.writerow(row)
+		
 """
 with open(r'daily3.csv','a') as csvfile:
 	spamwriter=csv.writer(csvfile)
-	spamwriter.writerow([])
-	spamwriter.writerow(['20170227','34','27'])
-"""					
+	spamreader=csv.reader(csvfile,delimiter=' ', quotechar='|')
+	for row in spamreader:
+		print row
+		if row[0] == date:
+			print 'hello world'
+	#spamwriter.writerow(['20170227','34','27'])
+					
+"""
